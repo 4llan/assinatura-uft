d54ee74d970717e95b2b1d0a3368a6d1ebc61184
Allan M. de Azevedo <4lln.medeiros@gmail.com>
2022-12-23 11:49:25 -0300

    Add page title as variable

--- src/app/assinatura/assinatura.component.ts
+++ src/app/assinatura/assinatura.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import {Title} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-assinatura',
@@ -10,19 +11,22 @@ import { map, shareReplay } from 'rxjs/operators';
 })
 export class AssinaturaComponent {
 
+  assinatura_tipo = 'individual';
+  titulo = 'Assinatura de e-mail institucional UFT'
+
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches),
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
-
-  assinatura_tipo = 'individual';
+  constructor(private breakpointObserver: BreakpointObserver, private titleService: Title) {
+    this.titleService.setTitle(this.titulo);
+  }
 
   copiarAssinatura() {
     document.addEventListener('copy', (e: ClipboardEvent) => {
-      let assinatura = document.getElementById("assinatura")?.innerHTML;
+      let assinatura = document.getElementById('assinatura')?.innerHTML;
       e.clipboardData?.setData('text/html', (assinatura!));
       e.preventDefault();
       document.removeEventListener('copy', null!);
